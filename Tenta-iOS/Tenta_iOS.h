ab4cc1fb3e7652e5dc357654b4bf983921199b5a
Niil Öhlin <niilohlin@gmail.com>
2018-12-06 13:19:04 +0100

    Add iOS target

---
+++ Tenta-iOS/Tenta_iOS.h
@@ -0,0 +1,19 @@
+//
+//  Tenta_iOS.h
+//  Tenta-iOS
+//
+//  Created by Niil Öhlin on 2018-12-06.
+//  Copyright © 2018 Niil Öhlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Tenta_iOS.
+FOUNDATION_EXPORT double Tenta_iOSVersionNumber;
+
+//! Project version string for Tenta_iOS.
+FOUNDATION_EXPORT const unsigned char Tenta_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Tenta_iOS/PublicHeader.h>
+
+
