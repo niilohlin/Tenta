cc24191e60576fe51c0c805507f19916a9e6403b
Niil Öhlin <niilohlin@gmail.com>
2018-12-10 17:23:28 +0100

    WIP

---
+++ Tenta/ModifyingGenerator.swift
@@ -0,0 +1,10 @@
+//
+// Created by Niil Öhlin on 2018-12-10.
+// Copyright (c) 2018 Niil Öhlin. All rights reserved.
+//
+
+import Foundation
+
+public struct ModifyingGenerator<ValueToTest> {
+    public let modify: (inout ValueToTest, Size, inout SeededRandomNumberGenerator) -> RoseTree<ValueToTest>
+}
